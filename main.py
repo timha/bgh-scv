e0a0f704ae7435cbd6434d87b86fc7c0c4475974
timha <dev@timha.io>
2022-01-15 21:38:33 -0800

    correct quotes. add welcome message

--- main.py
+++ main.py
@@ -8,25 +8,32 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 COMMAND_PREFIX = os.getenv("COMMAND_PREFIX")
+CH_WELCOME = os.getenv("CH_WELCOME")
+
+intents = discord.Intents.default()
+intents.members = True
 
 # set command prefix
-client = commands.Bot(command_prefix = COMMAND_PREFIX)
+client = commands.Bot(command_prefix = COMMAND_PREFIX, intents=intents)
 
 
 @client.event
 async def on_ready():
-    print('SVC ready to go sir!')
+    print('Map loaded')
     print('--------------------------')
 
 @client.command()
 async def hello(ctx):
-    await ctx.send('hello, SCV I am')
+    await ctx.send('SCV good to go sir!')
 
 # act member joins the server
 @client.event
 async def on_member_join(member):
-    await print('welcome' + str(member.name))
-
+    print("Member: " + str(member.id))
+    print("Joined: " + str(member.joined_at))
+    print("----------------------------------")
+    channel = client.get_channel(int(CH_WELCOME))
+    await channel.send("Welcome to Big Game Hunters, " + str(member.name) + "!")
 
 
 client.run(BOT_TOKEN)
