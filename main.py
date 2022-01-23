1ae98303fb69a76fade774243f10a2879b31ec71
timha <dev@timha.io>
2022-01-22 23:36:34 -0800

    add pause and resume functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pickle import TRUE
+from ast import alias
 import discord
 from discord.ext import commands
 from discord import FFmpegOpusAudio
@@ -22,7 +22,7 @@ intents = discord.Intents.all()
 
 client = commands.Bot(command_prefix = COMMAND_PREFIX, intents=intents)
 
-client.is_playing = False
+client.is_active = False
 
 @client.event
 async def on_ready():
@@ -49,23 +49,41 @@ async def on_voice_state_update(member, before, after):
     voice_channel = client.get_channel(int(CH_LOUNGE))
     welcome_channel = client.get_channel(int(CH_WELCOME))
     member_count = len(voice_channel.members)
-    if member_count > 0 and not client.is_playing:
-        client.is_playing = True
+    if member_count > 0 and not client.is_active:
+        client.is_active = True
         voice = await voice_channel.connect()
         source = FFmpegOpusAudio(SONG)
         audio_player = voice.play(source)
         print("starting music . . .")
-        await welcome_channel.send("START")
-        # await voice_channel.connect()
-    elif member_count == 1 and client.is_playing:
+        await welcome_channel.send("🎶🎶🎶 PLAYING 🎶🎶🎶")
+    elif member_count == 1 and client.is_active:
         await client.get_guild(int(GUILD_ID)).voice_client.disconnect()
 
 
 
 
-        client.is_playing = False
+        client.is_active = False
         print("stopping music . . .")
         await welcome_channel.send("STOP")
+
+# pause
+@client.command(aliases=['p'])
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send('SCV is not currently playing any tunes')
+
+# resume
+@client.command(aliases=['r'])
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send('SCV is already playing tunes')
+
 
 
 client.run(BOT_TOKEN)
