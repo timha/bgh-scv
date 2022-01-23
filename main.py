9e408b4dd76a76d4f1b425705f00c8ea8ed72be6
timha <dev@timha.io>
2022-01-22 21:27:55 -0800

    add functionality for bot to auto-join / disconnect from voice ch

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 COMMAND_PREFIX = os.getenv("COMMAND_PREFIX")
 CH_WELCOME = os.getenv("CH_WELCOME")
 CH_LOUNGE = os.getenv("CH_LOUNGE")
-
+GUILD_ID = os.getenv("GUILD_ID")
 
 
 
@@ -41,20 +41,24 @@ async def on_member_join(member):
     channel = client.get_channel(int(CH_WELCOME))
     await channel.send("Welcome to Big Game Hunters, " + str(member.name) + "!")
 
+
+# bot auto-join voice channel
 @client.event
 async def on_voice_state_update(member, before, after):
     voice_channel = client.get_channel(int(CH_LOUNGE))
     welcome_channel = client.get_channel(int(CH_WELCOME))
     member_count = len(voice_channel.members)
-    
+
     if member_count > 0 and not client.is_playing:
         client.is_playing = True
         print("starting music . . .")
         await welcome_channel.send("START")
-    elif member_count == 0 and client.is_playing:
+        await voice_channel.connect()
+    elif member_count == 1 and client.is_playing:
         client.is_playing = False
         print("stopping music . . .")
         await welcome_channel.send("STOP")
+        await client.get_guild(int(GUILD_ID)).voice_client.disconnect()
 
 
 client.run(BOT_TOKEN)
